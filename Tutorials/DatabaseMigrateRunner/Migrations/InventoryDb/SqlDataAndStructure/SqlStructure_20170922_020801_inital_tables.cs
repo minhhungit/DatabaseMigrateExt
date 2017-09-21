e86666c277939493a4a1e9b807337b6c36bd2b1a
Hung Vo <it.minhhung@gmail.com>
2017-09-22 02:10:42 +0700

    demo: add more database

---
+++ Tutorials/DatabaseMigrateRunner/Migrations/InventoryDb/SqlDataAndStructure/SqlStructure_20170922_020801_inital_tables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DatabaseMigrateExt;
+using DatabaseMigrateExt.Attributes;
+using FluentMigrator;
+
+namespace DatabaseMigrateRunner.Migrations.InventoryDb.SqlDataAndStructure
+{
+    [ExtMigration(scriptType: DatabaseScriptType.SqlDataAndStructure, year: 2017, month: 9, day: 22, hour: 02, minute: 08, second: 01)]
+    public class SqlStructure_20170922_020801_inital_tables : Migration
+    {
+        public override void Up()
+        {
+            if (!this.Schema.Schema("dbo").Table("Inventory").Exists())
+            {
+                Create.Table("Inventory").InSchema("dbo")
+                    .WithColumn("Id").AsInt32().Identity().PrimaryKey().NotNullable()
+                    .WithColumn("Test").AsString(200).NotNullable();
+            }
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
