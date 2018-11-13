ca12aa762777a9d8632ce55a2cdfee49df85243e
Hung Vo <it.minhhung@gmail.com>
2018-11-14 04:55:24 +0700

    fix codefactor

--- Tutorials/DatabaseMigrateRunner/Migrations/InventoryDb/TestInvalidScripts_DELETEME/invalid_migrate_version.cs
+++ Tutorials/DatabaseMigrateRunner/Migrations/InventoryDb/TestInvalidScripts_DELETEME/invalid_migrate_version.cs
@@ -11,44 +11,4 @@ namespace DatabaseMigrateRunner.Migrations.InventoryDb
 
         public override void Down() { throw new NotImplementedException(); }
     }
-
-    [ExtMgrDataStructure(2018, 25, 13, 01, 05, 07)]
-    public class invalid_migrate_version_month : Migration
-    {
-        public override void Up() { }
-
-        public override void Down() { throw new NotImplementedException(); }
-    }
-
-    [ExtMgrDataStructure(2018, 11, 62, 01, 05, 07)]
-    public class invalid_migrate_version_day : Migration
-    {
-        public override void Up() { }
-
-        public override void Down() { throw new NotImplementedException(); }
-    }
-
-    [ExtMgrDataStructure(2018, 11, 13, 44, 05, 07)]
-    public class invalid_migrate_version_hour : Migration
-    {
-        public override void Up() { }
-
-        public override void Down() { throw new NotImplementedException(); }
-    }
-
-    [ExtMgrDataStructure(2018, 11, 13, 01, 115, 07)]
-    public class invalid_migrate_version_minute : Migration
-    {
-        public override void Up() { }
-
-        public override void Down() { throw new NotImplementedException(); }
-    }
-
-    [ExtMgrDataStructure(2018, 11, 13, 01, 05, 523532)]
-    public class invalid_migrate_version_second : Migration
-    {
-        public override void Up() { }
-
-        public override void Down() { throw new NotImplementedException(); }
-    }
 }
